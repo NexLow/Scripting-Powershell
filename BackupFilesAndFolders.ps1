61733e3af2c4781394d9269fd1a3ce7703d556e4
NexLow <>
2020-07-22 17:33:59 +0200

    Update BackupFilesAndFolders.ps1

--- BackupFilesAndFolders.ps1
+++ BackupFilesAndFolders.ps1
@@ -1,7 +1,9 @@
 # This script is in WIP
 function CreateArchiveFilename {
-    [String]$date = Get-Date -UFormat %Y-%m-%d # Can be change by : %F
-    [String]$time = Get-Date -UFormat %H-%M
+    [String]$Date = Get-Date -UFormat %Y-%m-%d # Can be change by : %F
+    [String]$Time = Get-Date -UFormat %H-%M
+    [String]$FileName = $Date + "_" + $Time + "_test"
+    Write-Host "$FileName"
 }
 
 function Main {

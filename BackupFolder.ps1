88824684d9ff7a397727bd6aa206c282fbf2e688
NexLow <thibault.cholet@outlook.fr>
2020-07-26 12:36:24 +0200

    Release V1.0.0

--- BackupFolder.ps1
+++ BackupFolder.ps1
@@ -2,85 +2,116 @@
 .SYNOPSIS
 Backup the folder you want with the date in zip format.
 .DESCRIPTION
-WIP
+This script can work automatically with a scheduled task if it is correctly configured. Please test this script before use or automate.
+This script does not work with network paths. An absolute local path must be entered for variables $FolderToBackup and $BackupDestination.
 .NOTES
-This script was created by NexLow : https://github.com/NexLow
+Author : This script was created by NexLow : https://github.com/NexLow
+Creation date : 2020-07-22
+Latest review : 2020-07-26
+Version : V1.0.0
 #>
 
-# Setting the error action
-$ErrorActionPreference = 'Stop'
+function StartupMessage {
+    # Write a startup message.
+    # Get the name of the script.
+    $ScriptName = $($myInvocation.ScriptName).Split("\") | Select-Object -Last 1
+    Write-Host -Object "=================================================="
+    Write-Host -Object "Startup : $ScriptName" 
+    Write-Host -Object "=================================================="    
+}
 
-function CorrectRewritingOfVariables {
-    # Correct rewriting and checking if $FolderToBackup is valid
-    If (($FolderToBackup -eq "") -or ($FolderToBackup -match " ") -or ($FolderToBackup -match '^\\') -or ($FolderToBackup -match '^.\\')) {
-        Write-Host -Object 'Please, enter a valid absolute path for the variable "$FolderToBackup" and try again.' -ForegroundColor Red
+function SettingUpVariables {
+    # This is where you need to modify the variables for the script.
+    
+    # The folder you want to backup.
+    [String]$Script:FolderToBackup = "C:\Temp\FolderToBackup"
+    # The folder destination of the backup.
+    [String]$Script:BackupDestination = "C:\Temp\BackupDestination\"
+    # Set the name of the backup.
+    [String]$Script:NameOfBackupFile = "backup"
+    
+    # Show errors
+    $ErrorActionPreference = 'SilentlyContinue'
+    <#
+    Continue : is the default, and it tells the command to display an error message and continue to run.
+    SilentlyContinue : tells the command to display no error message, but to continue running.
+    Inquire : tells the command to display a prompt asking the user what to do.
+    Stop : tells the command to treat the error as terminating and to stop running.
+    #>
+}
+
+function VerifyFolderToBackup {
+    # Checking if $FolderToBackup is valid and correct rewriting.
+    Write-Host -Object 'Checking if $FolderToBackup is valid and correct rewriting : ' -NoNewline
+    If ($null -eq $FolderToBackup) {
+        Write-Host -Object 'Error ! This script needs a temp folder. Please, enter a valid absolute path for the variable "$FolderToBackup" and try again.' -ForegroundColor Red
+        Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+        Start-Sleep -Seconds 10
+        Exit
+    } elseif (($FolderToBackup -eq "") -or ($FolderToBackup -match " ") -or ($FolderToBackup -match '^\\') -or ($FolderToBackup -match '^.\\')) {
+        Write-Host -Object 'Error ! Please, enter a valid absolute path for the variable "$FolderToBackup" and try again.' -ForegroundColor Red
+        Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+        Start-Sleep -Seconds 10
         Exit
     } elseif ($FolderToBackup -notmatch '\\$') {
         [Int]$LengthFolderToBackup = $FolderToBackup.Length
         [String]$Script:FolderToBackup = $FolderToBackup.Insert($LengthFolderToBackup,"\")
     }
 
-    # Correct rewriting and checking if $BackupDestination is valid
-    If (($BackupDestination -eq "") -or ($BackupDestination -match " ") -or ($BackupDestination -match '^\\') -or ($BackupDestination -match '^.\\')) {
-        Write-Host -Object 'Please, enter a valid absolute path for the variable "$BackupDestination" and try again.' -ForegroundColor Red
+    # Test if the path is available. 
+    If (Test-Path -Path $FolderToBackup) { 
+        Write-Host -Object "The temporary folder `"$FolderToBackup`" exist and is valid." -ForegroundColor Green
+    } else {
+        Write-Host -Object "An error has occurred. Please, enter a valid absolute path for the variable `"`$FolderToBackup = $FolderToBackup`" and try again." -ForegroundColor Red
+        Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+        Start-Sleep -Seconds 10
+        Exit
+    }
+}
+
+function VerifyBackupDestination {
+    # Checking if $BackupDestination is valid and correct rewriting.
+    Write-Host -Object 'Checking if $BackupDestination is valid and correct rewriting : ' -NoNewline
+    If ($null -eq $BackupDestination) {
+        Write-Host -Object 'Error ! This script needs a temp folder. Please, enter a valid absolute path for the variable "$BackupDestination" and try again.' -ForegroundColor Red
+        Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+        Start-Sleep -Seconds 10
+        Exit
+    } elseif (($BackupDestination -eq "") -or ($BackupDestination -match " ") -or ($BackupDestination -match '^\\') -or ($BackupDestination -match '^.\\')) {
+        Write-Host -Object 'Error ! Please, enter a valid absolute path for the variable "$BackupDestination" and try again.' -ForegroundColor Red
+        Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+        Start-Sleep -Seconds 10
         Exit
     } elseif ($BackupDestination -notmatch '\\$') {
         [Int]$LengthBackupDestination = $BackupDestination.Length
         [String]$Script:BackupDestination = $BackupDestination.Insert($LengthBackupDestination,"\")
     }
-}
-
-function AreNotTheSame {
-    # Verification if the $BackupDestination is the same as $FolderToBackup
-    If ($BackupDestination -eq $FolderToBackup) {
-        Write-Host -Object 'The folder $BackupDestination is the same as $FolderToBackup. Please do not put the same.' -ForegroundColor Red
-        Exit
-    }
-}
-
-function VerifyFolderToBackup {
-    Write-Host
-    Write-Host -Object "=================================================="
-    Write-Host -Object "      Verification of the folder to backup"
-    Write-Host -Object "=================================================="
-
-    Write-Host "Verification of the existence of the folder : " -NoNewline
-    If (Test-Path -Path $FolderToBackup) {
-        Write-Host -Object "The folder `"$FolderToBackup`" exist" -ForegroundColor Green 
-    } Else {
-        Write-Host -Object "The folder `"$FolderToBackup`" does not exist" -ForegroundColor Red
-        Break
-    }
-
-}
-
-function VerifyBackupDestination {
-    Write-Host
-    Write-Host -Object "=================================================="
-    Write-Host -Object "  Verification of the destination backup folder"
-    Write-Host -Object "=================================================="
-
-    Write-Host "Verification of the existence of the folder : " -NoNewline
-    If (Test-Path -Path $BackupDestination) {
-        Write-Host -Object "The folder `"$BackupDestination`" exist." -ForegroundColor Green 
-    } Else {
+    
+    # Test if the path is available. 
+    If (Test-Path -Path $BackupDestination) { 
+        Write-Host -Object "The destination folder `"$BackupDestination`" exist and is valid." -ForegroundColor Green
+    } else {
         Write-Host -Object "The folder `"$BackupDestination`" does not exist." -ForegroundColor Red
         do {
             $CreateBackupDestination = Read-Host -Prompt "Do you want to create `"$BackupDestination`" (yes,no) ? "
             switch ($CreateBackupDestination.ToLower()) {
                 "yes" { 
-                    Write-Host -Object "Creating the destination backup folder : " -NoNewline
+                    Write-Host -Object 'Creation and verification if $BackupDestination is valid and correct rewriting : ' -NoNewline
                     New-Item -Path $BackupDestination -ItemType Directory > $null
                     If (Test-Path -Path $BackupDestination) { 
-                        Write-Host -Object "Ok" -ForegroundColor Green
+                        Write-Host -Object "The destination folder `"$BackupDestination`" exist and is valid." -ForegroundColor Green
                     } else {
                         Write-Host -Object 'An error has occurred. Please, enter a valid absolute path for the variable "$BackupDestination" and try again.' -ForegroundColor Red
+                        Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+                        Start-Sleep -Seconds 10
                         Exit
                     }
                 }
                 "no" { 
                     Write-Host -Object 'Please, enter a valid absolute path for the variable "$BackupDestination" and try again.' -ForegroundColor Red
-                    Exit
+                    Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+                    Start-Sleep -Seconds 10
+                    Exit                
                 }
                 Default { Write-Host -Object "Please, answers correctly..." -ForegroundColor Yellow }
             }
@@ -88,54 +119,103 @@ function VerifyBackupDestination {
     }
 }
 
+function AreNotTheSame {
+    # Verification if the $FolderToBackup is the same as $BackupDestination
+    Write-Host -Object 'Checking if $FolderToBackup and $BackupDestination are not the same : ' -NoNewline
+    If ($FolderToBackup -eq $BackupDestination) {
+        Write-Host -Object 'Warning ! The folder $FolderToBackup is the same as $BackupDestination.' -ForegroundColor Yellow
+        do {
+            $ContinueAsTheSame = Read-Host -Prompt 'Do you want to continue without changing variables (yes,no) ? '
+            switch ($ContinueAsTheSame.ToLower()) {
+                "yes" { 
+                    Continue
+                }
+                "no" { 
+                    Write-Host -Object 'So please, enter a different valid absolute path for variables "$FolderToBackup" and "$BackupDestination". You can retry after that.' -ForegroundColor Red
+                    Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+                    Start-Sleep -Seconds 10
+                    Exit
+                }
+                Default { Write-Host -Object "Please, answers correctly..." -ForegroundColor Yellow }
+            }
+        } until (($ContinueAsTheSame -match "yes") -or ($ContinueAsTheSame -match "no"))
+    } else {
+        Write-Host -Object "They are not the same. It is ok." -ForegroundColor Green
+    }
+}
+
 function CreateArchiveFilename {
+    # Create the name of the archive file that will be used for the backup file.
+    Write-Host "Creating Archive Filename : " -NoNewline
     Try {
-        Write-Host "Creating Archive Filename : " -NoNewline
         [String]$Date = Get-Date -UFormat %Y-%m-%d # Can be change by : %F
         [String]$Time = Get-Date -UFormat %H-%M
-        [String]$Filename = $Date + "_" + $Time + "_backup.zip" # Name to change
+        [String]$Filename = $Date + "_" + $Time + "_" + $NameOfBackupFile + ".zip"
         [String]$Script:FullFilename = $BackupDestination + $Filename
     }
     Catch {
         Write-Host -Object "An error has occurred. Please, try again." -ForegroundColor Red 
+        Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+        Start-Sleep -Seconds 10
         Exit
     }
     Finally {
-        Write-Host -Object "Ready..." -ForegroundColor Green
+        Write-Host -Object "Ready." -ForegroundColor Green
     }
 }
 
 function BackupFolder {
-    Write-Host
-    Write-Host -Object "=================================================="
-    Write-Host -Object "              Backuping folder"
-    Write-Host -Object "=================================================="
+    # Backup $FolderToBackup.
     Write-Host -Object "Creating the backup : " -NoNewline
 
-    # Check if the backup does not already exist
+    # Check if the backup does not already exist.
     If (Test-Path -Path $FullFilename) {
         Write-Host -Object "The backup `"$FullFilename`" already exist." -ForegroundColor Red 
+        Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+        Start-Sleep -Seconds 10
         Exit
     } Else {
         Compress-Archive -Path $FolderToBackup -DestinationPath $FullFilename > $null
         If (Test-Path -Path $FullFilename) {
             Write-Host -Object "The backup `"$FullFilename`" was created" -ForegroundColor Green 
         } Else {
-            Write-Host -Object "The backup `"$FullFilename`" does not exist" -ForegroundColor Red
+            Write-Host -Object "An error has occurred. The backup `"$FullFilename`" does not exist" -ForegroundColor Red
+            Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
+            Start-Sleep -Seconds 10
             Break
         }
     }
 }
 
+function EndMessage {
+    # Write an end message.
+    # Get the name of the script.
+    $ScriptName = $($myInvocation.ScriptName).Split("\") | Select-Object -Last 1
+    Write-Host -Object "=================================================="
+    Write-Host -Object "End of the script : $ScriptName" 
+    Write-Host -Object "=================================================="
+}
+
 function Main {
-    [String]$Script:FolderToBackup = "C:\Temp\FolderToBackup"
-    [String]$Script:BackupDestination = "C:\Temp\BackupDestination"
-    CorrectRewritingOfVariables
-    AreNotTheSame
-    VerifyBackupDestination
+    # Startup message.
+    StartupMessage
+    
+    # Setting up variables.
+    SettingUpVariables 
+
+    # Verification of variables.
     VerifyFolderToBackup
+    VerifyBackupDestination
+    AreNotTheSame
+    
+    # Create the archive filename.
     CreateArchiveFilename
+
+    # Backuping.
     BackupFolder
+
+    # End message.
+    EndMessage
 }
 
 # Run the script
--- NotifyModifyFiles.ps1
+++ NotifyModifyFiles.ps1
@@ -21,20 +21,20 @@ function SettingUpVariables {
     [Int]$Script:NbDaysDeleteCSVReport = "-0.5"
     # The folder you want to check.
     [String]$Script:FolderToCheck = "C:\Temp"
-    # Lists the files modified since -X days
+    # Lists the files modified since -X days.
     [Int]$Script:NbDays = "-1"
-    # Name of your CSV reoprt file
+    # Name of your CSV reoprt file.
     [String]$Script:NameCSV = "NewFilesDailyReport"
     
 
-    # Server mail configuration
+    # Server mail configuration.
     [String]$Script:SmtpUser = "test@test.test" # Your email address like : your@address.com
     [String]$Script:SmtpPassword = "testpasswordtest#" # Your password of your email address !!! Warning !!!
     [String]$Script:SmtpServer = "smtp-mail.outlook.com" # Your email server
     [String]$Script:SmtpPort = "587" # your port server
     #[String]$Script:SmtpPasswordSecure = $($SmtpPassword | ConvertTo-SecureString -AsPlainText -Force) 
 
-    # Mail configuration
+    # Mail configuration.
     # The $From must not be an anonymous address. 
     [String]$Script:From = "test@test.test" # Warning
     [String]$Script:To = "test@test.test" # Warning
@@ -51,8 +51,8 @@ function SettingUpVariables {
 }
 
 function StartupMessage {
-    # Write a startup message
-    # Get the name of the script
+    # Write a startup message.
+    # Get the name of the script.
     $ScriptName = $($myInvocation.ScriptName).Split("\") | Select-Object -Last 1
     Write-Host -Object "=================================================="
     Write-Host -Object "Startup : $ScriptName" 
@@ -60,7 +60,7 @@ function StartupMessage {
 }
 
 function VerifyTempFolder {
-    # Checking if $TempFolder is valid and correct rewriting
+    # Checking if $TempFolder is valid and correct rewriting.
     Write-Host -Object 'Checking if $TempFolder is valid and correct rewriting : ' -NoNewline
     If ($null -eq $TempFolder) {
         Write-Host -Object 'Error ! This script needs a temp folder. Please, enter a valid absolute path for the variable "$TempFolder" and try again.' -ForegroundColor Red
@@ -77,7 +77,7 @@ function VerifyTempFolder {
         [String]$Script:TempFolder = $TempFolder.Insert($LengthTempFolder,"\")
     }
 
-    # Test if the path is available 
+    # Test if the path is available. 
     If (Test-Path -Path $TempFolder) { 
         Write-Host -Object "The temporary folder `"$TempFolder`" exist and is valid." -ForegroundColor Green
     } else {
@@ -89,7 +89,7 @@ function VerifyTempFolder {
 }
 
 function VerifyNbDaysDeleteCSVReport {
-    # Check if $NbDaysDeleteCSVReport is valid
+    # Check if $NbDaysDeleteCSVReport is valid.
     Write-Host -Object 'Checking if $NbDaysDeleteCSVReport is valid : ' -NoNewline
     If ($null -eq $NbDaysDeleteCSVReport) {
         Write-Host -Object 'Error ! The variable $NbDaysDeleteCSVReport is empty or null. Please, enter a valid number less than "1" exemple "0" or "-1" and try agin.' -ForegroundColor Red
@@ -106,9 +106,8 @@ function VerifyNbDaysDeleteCSVReport {
     }
 }
 
-
 function VerifyFolderToCheck {
-    # Checking if $FolderToCheck is valid and correct rewriting
+    # Checking if $FolderToCheck is valid and correct rewriting.
     Write-Host -Object 'Checking if $FolderToCheck is valid and correct rewriting : ' -NoNewline
     If ($null -eq $FolderToCheck) {
         Write-Host -Object 'Error ! This script needs a temp folder. Please, enter a valid absolute path for the variable "$FolderToCheck" and try again.' -ForegroundColor Red
@@ -125,7 +124,7 @@ function VerifyFolderToCheck {
         [String]$Script:FolderToCheck = $FolderToCheck.Insert($LengthFolderToCheck,"\")
     }
 
-    # Test if the path is available 
+    # Test if the path is available.
     If (Test-Path -Path $FolderToCheck) { 
         Write-Host -Object "The folder `"$FolderToCheck`" exist and is valid." -ForegroundColor Green
     } else {
@@ -137,7 +136,7 @@ function VerifyFolderToCheck {
 }
 
 function VerifyNbDays {
-    # Check if $NbDays is valid
+    # Check if $NbDays is valid.
     Write-Host -Object 'Checking if $NbDays is valid : ' -NoNewline
     If ($null -eq $NbDays) {
         Write-Host -Object 'Error ! The variable $NbDays is empty or null. Please, enter a valid number less than "0" exemple "-1" and try agin.' -ForegroundColor Red
@@ -155,7 +154,7 @@ function VerifyNbDays {
 }
 
 function AreNotTheSame {
-    # Verification if the $FolderToCheck is the same as $TempFolder
+    # Verification if the $FolderToCheck is the same as $TempFolder.
     Write-Host -Object 'Checking if $FolderToCheck and $TempFolder are not the same : ' -NoNewline
     If ($FolderToCheck -eq $TempFolder) {
         Write-Host -Object 'Warning ! The folder $FolderToCheck is the same as $TempFolder.' -ForegroundColor Yellow
@@ -182,25 +181,25 @@ function AreNotTheSame {
 function GetListOfFiles {
     Write-Host -Object "Getting the list of new files : " -NoNewline
 
-    # Create the date variable
+    # Create the date variable.
     $Script:Date = (Get-Date).AddDays($NbDays)
     
-    # Create a list of all files
+    # Create a list of all files.
     $Script:ListOfAllFiles = Get-ChildItem -Path $Script:FolderToCheck -Recurse -File | Where-Object {$_.CreationTime -ge $Script:Date} | Sort-Object -Property Name
     
-    # Check if there are any files
+    # Check if there are any files.
     If ($null -eq $Script:ListOfAllFiles) {
-        # No new file, so exit the script
+        # No new file, so exit the script.
         Write-Host -Object "No new file since $Script:Date." -ForegroundColor Red
         Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
         Start-Sleep -Seconds 10
         Exit
     } else {
-        # Display the number of new files in the command prompt
+        # Display the number of new files in the command prompt.
         $Script:NbListOfFiles = $Script:ListOfAllFiles | Measure-Object | Select-Object -ExpandProperty Count
         Write-Host -Object "$Script:NbListOfFiles new files since $Script:Date." -ForegroundColor Green
         
-        # Display list of all files in the command prompt
+        # Display list of all files in the command prompt.
         #Foreach ($Script:Files in $Script:ListOfAllFiles) {
         #    Write-Host -Object "$Script:Files"
         #}
@@ -214,10 +213,10 @@ function CreateCSV {
     [String]$Script:FilenameCSV = $Script:DateFormat + "_" + $Script:TimeFormat + "_" + $Script:NameCSV + ".csv"
     [String]$Script:FullPathFileCSV = $Script:TempFolder + $Script:FilenameCSV
 
-    # Create the CSV report
+    # Create the CSV report.
     Write-Host -Object "Create the CSV report file : " -NoNewline
 
-    # Test if the file alreday existe
+    # Test if the file alreday existe.
     If (Test-Path -Path $FullPathFileCSV) {
         Write-Host -Object "The CSV report file `"$FullPathFileCSV`" already exists. Please, try again later." -ForegroundColor Red
         Write-Host -Object "Exit the script in 10 seconds..." -NoNewline
@@ -250,7 +249,7 @@ function SendEmail {
     $Smtp.Credentials = New-Object System.Net.NetworkCredential($SmtpUser, $SmtpPassword);
     $Smtp.Send($Mail);
 
-    # Check if the message has been sent successfully
+    # Check if the message has been sent successfully.
     if ($?) {
         Write-Host -Object "Email sent." -ForegroundColor Green
     } else {
@@ -262,13 +261,13 @@ function SendEmail {
 }
 
 function DeleteCSV {
-    # Delete old CSV report
+    # Delete old CSV report.
     $DateToDelete = $((Get-Date).AddDays($NbDaysDeleteCSVReport))
     Write-Host -Object "Delete old CSV report files since $DateToDelete : " -NoNewline
     $NumberOfOldCSVFile = Get-ChildItem -Path $TempFolder -Filter "*$NameCSV.csv" -File  | Where-Object { $_.LastWriteTime -lt $DateToDelete } | Measure-Object | Select-Object -ExpandProperty Count
     Get-ChildItem -Path $TempFolder -Filter "*$NameCSV.csv" -File  | Where-Object { $_.LastWriteTime -lt $DateToDelete } | Remove-Item
 
-    # Check if the delete has been execute successfully
+    # Check if the delete has been execute successfully.
     if ($?) {
         Write-Host -Object "$NumberOfOldCSVFile old files has been deleted." -ForegroundColor Green
     } else {
@@ -280,8 +279,8 @@ function DeleteCSV {
 }
 
 function EndMessage {
-    # Write an end message
-    # Get the name of the script
+    # Write an end message.
+    # Get the name of the script.
     $ScriptName = $($myInvocation.ScriptName).Split("\") | Select-Object -Last 1
     Write-Host -Object "=================================================="
     Write-Host -Object "End of the script : $ScriptName" 
@@ -289,34 +288,34 @@ function EndMessage {
 }
 
 function Main {
-    # Startup message
+    # Startup message.
     StartupMessage
 
-    # Setting up variables
+    # Setting up variables.
     SettingUpVariables 
 
-    # Verification of variables
+    # Verification of variables.
     VerifyTempFolder
     VerifyNbDaysDeleteCSVReport
     VerifyFolderToCheck
     VerifyNbDays
     AreNotTheSame
 
-    # Creating the list
+    # Creating the list.
     GetListOfFiles
 
-    # Creating the report in CSV
+    # Creating the report in CSV.
     CreateCSV
 
-    # Send the email
+    # Send the email.
     SendEmail
 
-    # Delete temp CSV files
+    # Delete temp CSV files.
     DeleteCSV
 
-    # End message
+    # End message.
     EndMessage
 }
 
-# Run the script
+# Run the script.
 Main
